3f22a3ef9f57a52ca5cacfe57d7fc97a37220292
kosk <mail@kosk.me>
2016-06-18 23:41:24 +0900

    Add method to import entries from NEologd

--- mbanana/resources/neologd.py
+++ mbanana/resources/neologd.py
@@ -4,7 +4,7 @@ from mbanana import db
 class NEologd(db.Model):
     __tablename__ = 'neologd'
     id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.Text, unique=True, nullable=False)
+    word = db.Column(db.Text, nullable=False)
     pron = db.Column(db.Text, index=True, nullable=False)
 
     def __repr__(self):
@@ -13,3 +13,22 @@ class NEologd(db.Model):
 
     def init():
         db.create_all()
+
+    def import_entries():
+        import glob
+
+        exclude_parts = frozenset(['組織', '記号', '代名詞'])
+        imported_words = set()
+
+        for filename in glob.glob('lib/mecab-ipadic-neologd/seed/*.csv'):
+            with open(filename, 'r', encoding='utf-8') as f:
+                for line in f:
+                    entry = line.strip().split(',')
+
+                    if entry[3] not in imported_words:
+                        imported_words.add(entry[3])
+                        if '名詞' in entry and \
+                                not exclude_parts.intersection(set(entry)):
+                            db.session.add(NEologd(word=entry[-3],
+                                                   pron=entry[-2]))
+                            db.session.commit()
