eb2b3f658eea206d592b2e9eb685a6febb8d9b52
kosk <mail@kosk.me>
2016-06-19 08:52:39 +0900

    Change dir structure to load app in right order

--- mbanana/__init__.py
+++ mbanana/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
-from flask_sqlalchemy import SQLAlchemy
-from gensim.models import Word2Vec
-from mbanana.resources.mbanana import MBanana
-
 app = Flask(__name__)
 app.config.from_object('mbanana.config')
 
+from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
+
+from gensim.models import Word2Vec
 w2v = Word2Vec.load('mbanana/w2v/jawiki.model')
 
+from mbanana.api.mbanana import MBanana
 api = Api(app)
 api.add_resource(MBanana, '/api/mbanana/', resource_class_args=[w2v])
