edc3975866bc9e1dd2c6cc5ec64340ea009345dc
kosk <mail@kosk.me>
2016-06-19 18:26:35 +0900

    Add config to avoid CORS

--- mbanana/__init__.py
+++ mbanana/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_object('mbanana.config')
+CORS(app)
 
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
