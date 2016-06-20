49ab3d5fe40b8f479574120961218685a05fca8f
kosk <mail@kosk.me>
2016-06-20 22:28:20 +0900

    Add unique key to column word

--- mbanana/models/neologd.py
+++ mbanana/models/neologd.py
@@ -4,7 +4,7 @@ from mbanana import db
 class NEologd(db.Model):
     __tablename__ = 'neologd'
     id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.Text, nullable=False)
+    word = db.Column(db.Text, unique=True, nullable=False)
     pron = db.Column(db.Text, index=True, nullable=False)
 
     def __repr__(self):
